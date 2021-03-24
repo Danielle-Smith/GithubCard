aeac28db7127929b67f77efc771e8cd6204c965b
danielle-smith <danielle@natesdesign.com>
2021-03-24 10:15:48 -0600

    using state = {}, onChange, and value instead of refs

--- githubCard.js
+++ githubCard.js
@@ -23,15 +23,21 @@ const testData = [
   }
   
   class Form extends React.Component {
-    userNameInput = React.createRef(); //using refs
+    state = { userName: '' };
     handleSubmit = (event) => {
       event.preventDefault();
-      console.log(this.userNameInput.current.value)
+      console.log(this.state.userName);
     };
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
-            <input type="text" placeholder="Github username" ref={this.userNameInput} required />
+            <input 
+            type="text" 
+            placeholder="Github username" 
+            value={this.state.userName} 
+            onChange={event => this.setState({ userName:event.target.value })} 
+            required 
+            />
             <button>Add card</button>
         </form>
       );
